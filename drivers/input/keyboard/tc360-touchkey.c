402782aae5df7b0bce80a0fb9c526417e0a0ac9b
Michael Richter <neldar@web.de>
2013-02-17 19:55:54 +0100

    device specific

--- drivers/input/keyboard/tc360-touchkey.c
+++ drivers/input/keyboard/tc360-touchkey.c
@@ -33,6 +33,10 @@
 #include <linux/wakelock.h>
 #include <linux/workqueue.h>
 #include <linux/uaccess.h>
+#include <linux/bln.h>
+
+#include <linux/regulator/consumer.h>
+
 
 #if defined(CONFIG_MACH_SERRANO)
 #if defined(CONFIG_MACH_SERRANO_SPR) || defined(CONFIG_MACH_SERRANO_USC) \
@@ -1582,6 +1586,69 @@ static struct attribute_group fac_attr_group = {
 };
 #endif
 
+#ifdef CONFIG_GENERIC_BLN
+
+struct tc360_data *bln_tc360_data;
+
+static int tc360_enable_touchkey_bln(int led_mask)
+{
+
+	bln_tc360_data->pdata->led_power(true);
+
+	return 0;
+}
+
+static int tc360_disable_touchkey_bln(int led_mask)
+{
+#if 0
+	i2c_smbus_write_byte_data(bln_tc360_data->client, TC360_CMD, TC360_CMD_LED_OFF);
+#endif
+	bln_tc360_data->pdata->led_power(false);
+
+	return 0;
+}
+
+
+static int tc360_power_on(void)
+{
+#if 0
+	struct regulator *r;
+
+	r = regulator_get(NULL, "v_led_3v3");
+
+	if (!IS_ERR(r)) {
+		r->rdev->constraints->state_mem.enabled = 1;
+		r->rdev->constraints->state_mem.disabled = 0;
+	}
+
+	bln_tc360_data->pdata->power(true);
+	msleep(TC360_POWERON_DELAY);
+	i2c_smbus_write_byte_data(bln_tc360_data->client, TC360_CMD, TC360_CMD_LED_ON);
+#endif
+	return 0;
+}
+
+static int tc360_power_off(void)
+{
+#if 0
+	struct regulator *r;
+	r = regulator_get(NULL, "v_led_3v3");
+	r->rdev->constraints->state_mem.enabled = 0;
+	r->rdev->constraints->state_mem.disabled = 1;
+	//bln_tc360_data->pdata->power(false);
+#endif
+	return 0;
+}
+
+static struct bln_implementation tc360_touchkey_bln = {
+	.enable = tc360_enable_touchkey_bln,
+	.disable = tc360_disable_touchkey_bln,
+	.power_on = tc360_power_on,
+	.power_off = tc360_power_off,
+	.led_count = 1
+};
+#endif
+
 static int tc360_init_interface(struct tc360_data *data)
 {
 	struct i2c_client *client = data->client;
@@ -1789,6 +1856,11 @@ static int __devinit tc360_probe(struct i2c_client *client,
 	data->early_suspend.suspend = tc360_early_suspend;
 	data->early_suspend.resume = tc360_late_resume;
 	register_early_suspend(&data->early_suspend);
+#endif
+
+#ifdef CONFIG_GENERIC_BLN
+	bln_tc360_data = data;
+	register_bln_implementation(&tc360_touchkey_bln);
 #endif
 
 	data->led_wq = create_singlethread_workqueue(client->name);
